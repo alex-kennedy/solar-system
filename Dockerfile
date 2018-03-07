c14df7093655fe4e84a7fbe76d3206a0313ce20c
alex-kennedy <apkennedy@me.com>
2018-03-08 01:06:01 +1300

    Added datastore update function for initial upload

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN pip install --trusted-host pypi.python.org -r astro/requirements.txt
 
 ENV GOOGLE_APPLICATION_CREDENTIALS /app/.credentials/site-update-back-end.json
 
-CMD ["python", "astro/asteroids/get_small_bodies.py"]
+CMD ["python", "-u", "astro/asteroids/get_small_bodies.py"]
 
 EXPOSE 80
--- astro/asteroids/get_small_bodies.py
+++ astro/asteroids/get_small_bodies.py
@@ -2,17 +2,52 @@ import os
 import requests
 from tqdm import tqdm
 import difflib
-from google.cloud import datastore, storage
+from google.cloud import datastore, storage, bigquery
 import time
+import datetime
 
-
+# TODO Add all these to a config file
 URL = 'http://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT'
-SAVE_FILE = ""
+SAVE_FILE = 'astro/asteroids/'
 DOWNLOADED = False
 GCLOUD_STORAGE_BUCKET = 'asteroid-data'
+BIGQUERY_DATASET_ID = 'asteroids_data'
+
+# Formatting from: https://www.minorplanetcenter.net/iau/info/MPOrbitFormat.html
+SCHEMA = [
+    #['name', (.dat col start, .dat col end), 'bigquery field', datastore indexed? ]
+    ['designation', (1, 7), 'STRING', True],
+    ['h', (9, 13), 'FLOAT', False],
+    ['g', (15, 19), 'FLOAT', False],
+    ['epoch', (21, 25), 'DATETIME', False],
+    ['m', (27, 35), 'FLOAT', False],
+    ['perihelion', (38, 46), 'FLOAT', False],
+    ['node', (49, 57), 'FLOAT', False],
+    ['incl', (60, 68), 'FLOAT', False],
+    ['e', (71, 79), 'FLOAT', True],
+    ['n', (81, 91), 'FLOAT', False],
+    ['a', (93, 103), 'FLOAT', True],
+    ['u', (106, 106), 'FLOAT', True],
+    ['reference', (108, 116), 'STRING', False],
+    ['observations', (118, 122), 'INTEGER', False],
+    ['oppositions', (124, 126), 'INTEGER', False],
+    ['arc_year_or_length', (128, 131), 'FLOAT', False],
+    ['arc_year_last', (128, 131), 'INTEGER', False],
+    ['flags', (162, 165), 'STRING', False],
+    ['readable_designation', (167, 194), 'STRING', True],
+    ['last_observation', (195, 202), 'STRING', False],
+    ['u_flag', None, 'STRING', True],
+    ['orbit_type', None, 'STRING', True],
+    ['neo', None, 'BOOLEAN', True],
+    ['neo_1km', None, 'BOOLEAN', True],
+    ['opposition_seen_earlier', None, 'BOOLEAN', False],
+    ['critical_list_numbered', None, 'BOOLEAN', True],
+    ['pha', None, 'BOOLEAN', True]
+]
 
 
 def join_list(list, sep=','):
+    """Combine a list of objects into one string separated by sep."""
     string = ''
     for item in list:
         string += str(item) + sep
@@ -23,29 +58,18 @@ def join_list(list, sep=','):
     return string
 
 
-def get_col_names(line_format, processed_fields):
-    col_names = []
-
-    for col in line_format:
-        col_names.append(col[0])
-
-    for function_call in processed_fields:
-        for col in function_call[0]:
-            col_names.append(col)
-
-    return col_names
-
-
 def write_header(out_file, col_names):
+    """Add a header to the .csv outfile, determined by the list col_names"""
     header_string = join_list(col_names)
     out_file.write(header_string + '\n')
 
 
 def line_to_list(line_format, line):
+    """Convert a formatted string line by the line_format"""
     separate_values = []
     for col in line_format:
         lower = col[1][0] - 1 # -1 due to indexing
-        upper = col[1][1]     # -1 due to indexing, +1 due to convert to exclusive indexing
+        upper = col[1][1]     # -1 due to indexing, +1 to convert to exclusive indexing
 
         value = line[lower:upper].strip()
         separate_values.append(value)
@@ -53,7 +77,27 @@ def line_to_list(line_format, line):
     return separate_values
 
 
+def parse_line(line):
+    """Convert a list of strings to their types as specified in the schema"""
+    assert type(line) is list
+    assert len(line) == len(SCHEMA)
+
+    for i in range(len(line)):
+        try:
+            if SCHEMA[i][2] == 'FLOAT':
+                line[i] = float(line[i])
+            elif SCHEMA[i][2] == 'INTEGER':
+                line[i] = int(line[i])
+            elif SCHEMA[i][2] == 'BOOLEAN':
+                line[i] = bool(int(line[i]))
+            elif SCHEMA[i][2] == 'DATETIME':
+                line[i] = datetime.datetime.strptime(line[i], '%Y-%m-%d')
+        except ValueError:
+            line[i] = None
+
+
 def unpack_designation(col_names, values):
+    """Unpack the asteroid designation in the list of values, MPC format"""
     index = col_names.index("designation")
     designation  = values[index]
 
@@ -121,7 +165,9 @@ def unpack_uncertainty_parameter(col_names, values):
         values[u_index] = '-1'
         values.append(u)
 
+
 def unpack_epoch(col_names, values):
+    """Unpack epoch to date based on MPC format"""
     epoch_index = col_names.index('epoch')
     epoch_packed = values[epoch_index]
     epoch_unpacked = ''
@@ -230,29 +276,8 @@ def gcloud_download_previous():
 
 
 def process_small_bodies():
-    # Formatting from: https://www.minorplanetcenter.net/iau/info/MPOrbitFormat.html
-    line_format = [
-        ['designation', (1, 7)],
-        ['h', (9, 13)],
-        ['g', (15, 19)],
-        ['epoch', (21, 25)],
-        ['m', (27, 35)],
-        ['perihelion', (38, 46)],
-        ['node', (49, 57)],
-        ['incl', (60, 68)],
-        ['e', (71, 79)],
-        ['n', (81, 91)],
-        ['a', (93, 103)],
-        ['u', (106, 106)],
-        ['reference', (108, 116)],
-        ['observations', (118, 122)],
-        ['oppositions', (124, 126)],
-        ['arc_year_or_length', (128, 131)],
-        ['arc_year_last', (128, 131)],
-        ['flags', (162, 165)],
-        ['readable_designation', (167, 194)],
-        ['last_observation', (195, 202)]
-    ]
+    line_format = [field[0:2] for field in SCHEMA if field[1] is not None]
+    col_names = [field[0] for field in SCHEMA]
 
     processed_fields = [
         # [('new1', 'new2', ...), function_pointer]
@@ -262,10 +287,6 @@ def process_small_bodies():
         [('orbit_type', 'neo', 'neo_1km', 'opposition_seen_earlier', 'critical_list_numbered', 'pha'), unpack_flags]
     ]
 
-    col_names = get_col_names(line_format, processed_fields)
-
-    skip_rows = 43
-
     with open(SAVE_FILE + "asteroids.dat", "r") as dat, \
          open(SAVE_FILE + "asteroids.csv", "w") as csv:
 
@@ -273,7 +294,8 @@ def process_small_bodies():
 
         write_header(csv, col_names)
 
-        for i in range(skip_rows):
+        # Skip the 43 header rows in the .dat file
+        for i in range(43):
             dat.readline()
 
         for line in tqdm(dat, unit='lines'):
@@ -302,7 +324,7 @@ def check_for_changes():
         old = old.readlines()
 
 
-    diff = difflib.unified_diff(old, new, n=0)      # provide no context (n=0)
+    diff = difflib.unified_diff(new, old, n=0)      # provide no context (n=0)
     count = 0
     with open(SAVE_FILE + "overwrites.csv", 'w') as outfile:
         for diff_line in diff:
@@ -315,7 +337,7 @@ def check_for_changes():
                 outfile.write(line)
     print("{0} write operations to be made.".format(count))
 
-    diff = difflib.unified_diff(old, new, n=0)      # reset diff generator
+    diff = difflib.unified_diff(new, old, n=0)      # reset diff generator
     count = 0
     with open(SAVE_FILE + "deletions.csv", 'w') as outfile:
         for diff_line in diff:
@@ -335,7 +357,7 @@ def check_for_changes():
     #         outfile.write(line)
 
 
-def gcloud_overwrite_previous():
+def glcoud_update_storage():
     client = storage.Client()
     bucket = client.bucket(GCLOUD_STORAGE_BUCKET)
     assert bucket.exists()
@@ -346,8 +368,86 @@ def gcloud_overwrite_previous():
     print("Successfully wrote CSV to google cloud.")
 
 
+def read_in_chunks(file_object, num_lines=500):
+    while True:
+        chunk = []
+        for _ in range(num_lines):
+            data = file_object.readline()
+
+            if data:
+                chunk.append(data)
+            else:
+                break
+
+        if len(chunk) == 0:
+            break
+
+        yield chunk
+
+
 def gcloud_update_datastore():
-    pass
+    client = datastore.Client()
+
+    # Get CSV schema
+    schema = [field[0] for field in SCHEMA]
+
+    count = 0
+
+    with open(SAVE_FILE + 'asteroids.csv') as overwrites_file_object, \
+        open(SAVE_FILE + 'successful.csv', 'a') as successful:
+
+        overwrites_file_object.readline() # ignore header FOR NOW
+
+        overwrites = read_in_chunks(overwrites_file_object, num_lines=500)
+        for chunk in overwrites:
+            tasks = []
+            for line in chunk:
+                line = line.strip().split(',')
+                parse_line(line)
+
+                asteroid = dict(zip(schema, line))
+                task_key = client.key('asteroid', asteroid['designation'])
+                del asteroid['designation']
+
+                task = datastore.Entity(
+                    key=task_key,
+                    exclude_from_indexes=[field[0] for field in SCHEMA if field[3] is False])
+                task.update(asteroid)
+                tasks.append(task)
+
+            # Put to datastore
+            if len(tasks) == 1:
+                client.put(tasks[0])
+            else:
+                client.put_multi(tasks)
+            successful.write(''.join(chunk))
+            count += len(tasks)
+            print("Placed {} entities to Google Datastore ({} total)".format(len(tasks), count))
+
+
+def gcloud_update_bigquery():
+    client = bigquery.Client()
+
+    gcloud_storage_reference = 'gs://{0}/{1}'.format(GCLOUD_STORAGE_BUCKET, 'asteroids.csv')
+    dataset = client.dataset(BIGQUERY_DATASET_ID)
+
+    job_config = bigquery.LoadJobConfig()
+    job_config.source_format = 'CSV'
+    job_config.skip_leading_rows = 1
+    job_config.schema = [bigquery.SchemaField(field[0], field[2]) for field in SCHEMA]
+
+    # Delete and reload
+    client.delete_table(dataset.table('asteroids'))
+    load_job = client.load_table_from_uri(
+        gcloud_storage_reference,
+        dataset.table('asteroids'),
+        job_config=job_config
+    )
+
+    print('Beginning load job from Cloud Storage to BigQuery...')
+    load_job.result()  # Waits for table load to complete.
+    assert load_job.state == 'DONE'
+    print('BigQuery job completed successfully.')
 
 
 def clean_files():
@@ -359,15 +459,22 @@ def clean_files():
 
 
 def update_site():
-    download_latest() #X
-    gcloud_download_previous() #X
-    process_small_bodies() #X
-    check_for_changes()
-    #gcloud_update_datastore()
-    gcloud_overwrite_previous() #X
-    #clean_files() #X
-    pass
+    print('Beginning update of site backend...\n')
+    start = time.time()
+
+    #download_latest() #X
+    #gcloud_download_previous() #X
+    #process_small_bodies() #X
+    #check_for_changes()
+    gcloud_update_datastore()
+    #glcoud_update_storage()
+    #gcloud_update_bigquery()
+
+    end = time.time()
+    print('\nUpdate of site backend completed successfully in {}:{}. '.format(round((end - start) // 60), round((end - start) % 60)))
 
 
 if __name__ == '__main__':
     update_site()
+
+    pass
