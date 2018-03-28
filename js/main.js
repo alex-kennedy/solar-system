af3eb47615ee9d249e3854d2e421af28aa80b0e4
alex-kennedy <apkennedy@me.com>
2018-03-29 02:37:00 +1300

    First inclusion of planets (incorrect)

--- js/main.js
+++ js/main.js
@@ -16,7 +16,9 @@ function init() {
     addControls();
     addStats();
     addBrightStars();
+    // addCelestialSphereWireframe();
     addPlanets();
+    addSun();
 
     console.timeEnd();
 }
@@ -140,20 +142,60 @@ function addBrightStars() {
 
 
 function addPlanets() {
-    var planets = [];
-
     $.get('astro/planets/planetary_elements.json', processPlanets);
     function processPlanets(data) {
+        var planets = [];
 
         for (var system_name in data) {
+
             if (data.hasOwnProperty(system_name)) {
                 planets.push(new Planet(system_name, data[system_name]));
             }
+
         }
 
-        console.log(planets);
+        var geometry = new THREE.Geometry();
+
+        for ( var i = 0; i < planets.length; i ++ ) {
+
+            var vertex = new THREE.Vector3();
+            vertex.x = planets[i].currentCoords[0];
+            vertex.y = planets[i].currentCoords[1];
+            vertex.z = planets[i].currentCoords[2];
+
+            geometry.vertices.push(vertex);
+
+        }
+
+        var size = 4;
+        var material = new THREE.PointsMaterial( {size: size} );
+        material.color.setRGB(0, 0, 1);
+        var particles = new THREE.Points( geometry, material );
+
+        scene.add(particles);
+        console.log('apparently added');
 
     }
+}
+
+
+function addSun() {
+
+    var geometry = new THREE.Geometry();
+
+    var vertex = new THREE.Vector3();
+    vertex.x = 0;
+    vertex.y = 0;
+    vertex.z = 0;
+
+    geometry.vertices.push(vertex);
+
+    var material = new THREE.PointsMaterial( {size: 1} );
+    material.color.setRGB(1, 0.25, 0);
+    var particles = new THREE.Points( geometry, material );
+
+    scene.add(particles);
+
 }
 
 
@@ -170,8 +212,6 @@ function getCenturiesTT() {
 }
 
 
-
-
 function OrbitingObject(name, a, e, I, L, long_peri, long_node) {
     this.name = name || "";
     this.a = a;
@@ -183,20 +223,25 @@ function OrbitingObject(name, a, e, I, L, long_peri, long_node) {
 
     this.solveKepler = function (tol) {
         // Currently only solves in degrees
-        tol = typeof tol !== 'undefined' ? tol : 1.e-5;
+        tol = typeof tol !== 'undefined' ? tol : 1e-5;
 
         var e_star = (180 / Math.PI) * this.e;
         var E_n = this.M + e_star * Math.sin((Math.PI / 180) * this.M);
         var delta = 360;
+        var count = 0;
 
-        while (Math.abs(delta) < tol) {
+        while (Math.abs(delta) > tol) {
             delta = (this.M - (E_n - e_star * Math.sin((Math.PI / 180) * E_n))) / (1 - this.e * Math.cos((Math.PI / 180) * E_n));
             E_n = E_n + delta;
+            count = count + 1;
         }
+
+        this.E = E_n;
 
         return E_n;
 
     };
+
 }
 
 OrbitingObject.prototype = {
@@ -219,8 +264,34 @@ function Planet(name, elements) {
     OrbitingObject.call(this, name, elements['a'], elements['e'], elements['I'], elements['L'], elements['long_peri'], elements['long_node']);
 
     this.arg_peri = this.long_peri - this.long_node;
-    this.M = ((this.L - this.long_peri - 180) % 360) + 180;
+    this.M = ((this.L - this.long_peri + 180) % 360) - 180;
 
+    this.transformToGeocentric = function (true_anomaly) {
+        true_anomaly = true_anomaly || this.E || 0;
+
+        var x = this.a * (Math.cos((Math.PI/180) * true_anomaly) - this.e);
+        var y = this.a * Math.sqrt(1 - Math.pow(this.e, 2));
+
+        var conv = Math.PI/180;
+
+        var co = Math.cos(conv * this.arg_peri);
+        var cO = Math.cos(conv * this.long_node);
+        var cI = Math.cos(conv * this.I);
+        var so = Math.sin(conv * this.arg_peri);
+        var sO = Math.sin(conv * this.long_node);
+        var sI = Math.sin(conv * this.I);
+
+        var x_ecl = (co*cO - so*sO*cI) * x + (-so*cO - co*sO*cI) * y;
+        var y_ecl = (co*sO + so*cO*cI) * x + (-so*sO + co*cO*cI) * y;
+        var z_ecl = (so*sI) * x + (co*sI) * y;
+
+        this.currentCoords = [x_ecl, y_ecl, z_ecl];
+        return this.currentCoords;
+
+    };
+
+    this.solveKepler();
+    this.transformToGeocentric();
 
 }
 
