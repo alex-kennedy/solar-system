3ca16c00a97da9aee410f5fbb30a1c5438ec1e0b
Alex Kennedy <apkennedy@me.com>
2021-07-18 01:03:40 +1000

    Update asteroid front-end for new rust module

--- app/src/components/AsteroidSet.js
+++ app/src/components/AsteroidSet.js
@@ -4,40 +4,32 @@ import * as wasm from "../wasm/index";
 wasm.debug_init();
 
 class AsteroidSet {
-  constructor(asteroidOrbits, epoch) {
-    this.count = asteroidOrbits.length;
-    this.asteroidSet = wasm.AsteroidSet.new(asteroidOrbits.length, epoch);
-    const orbitsBuffer = this.orbitsBuffer();
-    for (let i = 0; i < asteroidOrbits.length; i++) {
-      const asteroid = asteroidOrbits[i];
-      for (let orbitalElement = 0; orbitalElement < 6; orbitalElement++) {
-        orbitsBuffer[i * 6 + orbitalElement] = asteroid[orbitalElement];
-      }
-    }
+  constructor(internal) {
+    this.internal = internal;
     this.locationsComputed = false;
   }
 
+  static withCapacity(capacity) {
+    return new AsteroidSet(wasm.AsteroidSet.with_capacity(capacity));
+  }
+
+  setEpoch(epoch) {
+    this.internal.set_epoch(epoch);
+  }
+
   recomputeLocations(t) {
-    this.asteroidSet.recompute_locations(t);
+    this.internal.recompute_locations(t);
     this.locationsComputed = true;
   }
 
-  orbitsBuffer() {
-    return new Float32Array(
-      memory.buffer,
-      this.asteroidSet.asteroids_ptr(),
-      this.count * 6
-    );
-  }
-
-  locationsBuffer() {
+  locations() {
     if (this.locationsComputed === false) {
-      console.warn("Locations have not been computed!")
+      console.warn("Locations have not been computed!");
     }
     const buffer = new Float32Array(
       memory.buffer,
-      this.asteroidSet.locations_ptr(),
-      this.count * 3
+      this.internal.locations_ptr(),
+      this.internal.length() * 3
     );
     return buffer;
   }
