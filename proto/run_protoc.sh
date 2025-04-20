6006229cf59b0cfed070c33f253d25836963b1fc
Alex Kennedy <apkennedy@me.com>
2025-04-20 23:07:26 +1000

    Move web app under a web directory to re-organise for next.js

--- proto/run_protoc.sh
+++ proto/run_protoc.sh
@@ -1,8 +1,8 @@
 # Proto definitions are *not* generated on the fly, they must be kept up to 
 # date by running this.
 
-protoc --plugin="./node_modules/.bin/protoc-gen-ts_proto" \
-  --ts_proto_out="./app/src/lib" \
+protoc --plugin="./web/node_modules/.bin/protoc-gen-ts_proto" \
+  --ts_proto_out="./web/app/src/lib" \
   --go_out="./proto/go" \
   "./proto/bright_stars.proto" \
   "./proto/asteroids.proto"
